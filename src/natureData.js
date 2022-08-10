6c478b6abe36ce9f5b0a39201bcdf331e4966dbd
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2022-08-10 16:54:39 -0700

    nature data file added

---
+++ src/natureData.js
@@ -0,0 +1,96 @@
+export default [
+    {
+        "page": 1,
+        "per_page": 15,
+        "photos": [
+            {
+                "id": 15286,
+                "width": 2500,
+                "height": 1667,
+                "url": "https://www.pexels.com/photo/person-walking-between-green-forest-trees-15286/",
+                "photographer": "Luis del Río",
+                "photographer_url": "https://www.pexels.com/@luisdelrio",
+                "photographer_id": 1081,
+                "avg_color": "#283419",
+                "src": {
+                    "original": "https://images.pexels.com/photos/15286/pexels-photo.jpg",
+                    "large2x": "https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+                    "large": "https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=650&w=940",
+                    "medium": "https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350",
+                    "small": "https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=130",
+                    "portrait": "https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&fit=crop&h=1200&w=800",
+                    "landscape": "https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200",
+                    "tiny": "https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=200&w=280"
+                },
+                "liked": false,
+                "alt": "Person Walking Between Green Forest Trees"
+            },
+            {
+                "id": 3408744,
+                "width": 3546,
+                "height": 2255,
+                "url": "https://www.pexels.com/photo/scenic-view-of-snow-capped-mountains-during-night-3408744/",
+                "photographer": "stein egil liland",
+                "photographer_url": "https://www.pexels.com/@therato",
+                "photographer_id": 144244,
+                "avg_color": "#557088",
+                "src": {
+                    "original": "https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg",
+                    "large2x": "https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+                    "large": "https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+                    "medium": "https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&h=350",
+                    "small": "https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&h=130",
+                    "portrait": "https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=1200&w=800",
+                    "landscape": "https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200",
+                    "tiny": "https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=200&w=280"
+                },
+                "liked": false,
+                "alt": "Scenic View Of Snow Capped Mountains During Night"
+            },
+            {
+                "id": 624015,
+                "width": 4216,
+                "height": 2848,
+                "url": "https://www.pexels.com/photo/aurora-borealis-624015/",
+                "photographer": "Frans van Heerden",
+                "photographer_url": "https://www.pexels.com/@frans-van-heerden-201846",
+                "photographer_id": 201846,
+                "avg_color": "#71839F",
+                "src": {
+                    "original": "https://images.pexels.com/photos/624015/pexels-photo-624015.jpeg",
+                    "large2x": "https://images.pexels.com/photos/624015/pexels-photo-624015.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+                    "large": "https://images.pexels.com/photos/624015/pexels-photo-624015.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+                    "medium": "https://images.pexels.com/photos/624015/pexels-photo-624015.jpeg?auto=compress&cs=tinysrgb&h=350",
+                    "small": "https://images.pexels.com/photos/624015/pexels-photo-624015.jpeg?auto=compress&cs=tinysrgb&h=130",
+                    "portrait": "https://images.pexels.com/photos/624015/pexels-photo-624015.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=1200&w=800",
+                    "landscape": "https://images.pexels.com/photos/624015/pexels-photo-624015.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200",
+                    "tiny": "https://images.pexels.com/photos/624015/pexels-photo-624015.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=200&w=280"
+                },
+                "liked": false,
+                "alt": "Aurora Borealis"
+            },
+            {
+                "id": 572897,
+                "width": 6914,
+                "height": 4463,
+                "url": "https://www.pexels.com/photo/mountain-covered-snow-under-star-572897/",
+                "photographer": "eberhard grossgasteiger",
+                "photographer_url": "https://www.pexels.com/@eberhardgross",
+                "photographer_id": 121938,
+                "avg_color": "#5D5A63",
+                "src": {
+                    "original": "https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg",
+                    "large2x": "https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+                    "large": "https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+                    "medium": "https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&h=350",
+                    "small": "https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&h=130",
+                    "portrait": "https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=1200&w=800",
+                    "landscape": "https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200",
+                    "tiny": "https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=200&w=280"
+                },
+                "liked": false,
+                "alt": "Mountain Covered Snow Under Star"
+            }
+        ]
+    }
+]
