59ab2ea7ebb7c435ff7ec0da294c06c566b82f51
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2022-08-10 17:41:18 -0700

    added the logo and icon, navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>working</h1>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../images/logo.jpg"
+
+
+function NavBar(){
+    return(
+        <nav>
+            <img src={logo}/>
+        </nav>
+    )
+}
+
+export default NavBar;
